95ff4dbf19a916fd7c6408254f46f5c6fd1e2fbf
TakahiroFujitaSJB <TakahiroTanakaSJB@gmail.com>
2024-07-12 12:38:30 -0600

    New React Project checkpoint 1

---
+++ src/pages/homePage/homePage.jsx
@@ -0,0 +1,17 @@
+import './homePage.scss'
+
+function HomePage() {
+    return (
+        <div className='homePage'>
+            <div className='textContainer'>  
+            
+            </div>
+
+            <div className='imgContainer'>  
+                <img src="bg.png" alt="" />
+            </div> 
+        </div>
+    )
+}
+
+export default HomePage
