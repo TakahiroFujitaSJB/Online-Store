534683833188d2157dda688c81841ed8990ba2ab
TakahiroFujitaSJB <TakahiroTanakaSJB@gmail.com>
2024-07-19 10:37:56 -0600

    Update Slider.jsx

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,37 +4,38 @@ import Arrow from "../../assets/img/arrow.png";
 
 function Slider ({images}) {
     const [imageIndex, setImageIndex] = useState(null);
-    const changeSlide = direction => {
-        if(direction==="left"){
-            if(imageIndex === 0) {
-                setImageIndex(images.length - 1);
-            }
-            else {
-                setImageIndex(imageIndex - 1);
-            }
+    const changeSlideR = direction => { 
+        
+        if(imageIndex === images.length-1) {
+            setImageIndex(0);
         }
-        else{
-            if(imageIndex === images.length-1) {
-                setImageIndex(0);
-            }
-            else {
-                setImageIndex(imageIndex + 1);
-            }
+        else {
+            setImageIndex(imageIndex + 1);
         }
+    }
+
+    const changeSlideL = direction => {
+        if(imageIndex === 0) {
+            setImageIndex(images.length - 1);
+        }
+        else {
+            setImageIndex(imageIndex - 1);
+        }
+        
     }
 
     return (
     <div className="slider"> 
         {imageIndex !=null && <div className='fullSlider'>
             <div className='arrow'>
-                <img src={Arrow} alt="" />
+                <img src={Arrow} className='left' onClick={changeSlideL} alt="" />
             </div>
             <div className='imageContainer'>
                 <img src={images[imageIndex]} alt="" />
              
             </div>
             <div className='arrow'>
-                <img src={Arrow} alt="" className='right' onClick={changeSlide}  />
+                <img src={Arrow} alt="" className='right' onClick={changeSlideR}  />
             </div>
             <div className='close' onClick={() => setImageIndex(null)}>
                 x
